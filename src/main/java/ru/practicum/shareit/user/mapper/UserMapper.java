574cb097b0b61177c494a0b1a42a62d46453033f
Василиса <vasilisaraist@yandex.ru>
2023-07-01 21:02:14 +0300

    - убраны импорты;

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.user.mapper;
 
-import org.springframework.stereotype.Component;
+
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
