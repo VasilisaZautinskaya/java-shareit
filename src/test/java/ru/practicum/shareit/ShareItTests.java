77711c0a357a0aa87745374ffdcebffc1fc88b23
Admin <vasilisaraist@yandex.ru>
2023-07-12 12:10:02 +0300

    - booking;

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,13 +1,13 @@
 package ru.practicum.shareit;
 
-import org.junit.jupiter.api.Disabled;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
+
 @SpringBootTest
 class ShareItTests {
-    @Disabled
+
     @Test
     void contextLoads() {
     }
