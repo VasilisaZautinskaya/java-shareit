4735826f168a0dada053d2b4749fa13990e7c9ac
Василиса <vasilisaraist@yandex.ru>
2023-07-14 23:13:14 +0300

    - убрала импорты;

--- src/main/java/ru/practicum/shareit/item/dto/UpdateItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/UpdateItemDto.java
@@ -5,8 +5,6 @@ import lombok.Data;
 import ru.practicum.shareit.item.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Builder
 @Data
