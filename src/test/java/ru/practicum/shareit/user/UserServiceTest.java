8e7dcb3f07f37e6a9811767b71e4e90e8807b239
Василиса <vasilisaraist@yandex.ru>
2023-07-21 00:59:50 +0300

    - userUpdate;

--- src/test/java/ru/practicum/shareit/user/UserServiceTest.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceTest.java
@@ -14,6 +14,9 @@ import ru.practicum.shareit.user.Service.UserService;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -52,7 +55,7 @@ public class UserServiceTest {
         User user = UserTestData.getUserTwo();
         user.setEmail("dodo@example.com");
 
-        RuntimeException throwable = (RuntimeException )  Assertions.catchThrowable(() -> userService.update(user.getId(), user));
+        RuntimeException throwable = (RuntimeException) Assertions.catchThrowable(() -> userService.update(user.getId(), user));
 
         Assertions.assertThat(throwable)
                 .hasMessageStartingWith("Пользователь с таким email уже зарегистрирован")
@@ -60,9 +63,9 @@ public class UserServiceTest {
     }
 
     @Test
-    void updateUser() {
+    void updateUserNullId() {
         User user = UserTestData.getUserOne();
-        RuntimeException throwable = (RuntimeException )  Assertions.catchThrowable(() -> userService.update(user.getId(), user));
+        RuntimeException throwable = (RuntimeException) Assertions.catchThrowable(() -> userService.update(user.getId(), user));
 
         Assertions.assertThat(throwable)
                 .hasMessageStartingWith("Не найден пользователь")
@@ -70,5 +73,46 @@ public class UserServiceTest {
 
     }
 
+    @Test
+    void updateUser() {
+        User user = UserTestData.getUserOne();
+        String name = "NameUserOne";
+        String email = "dodo@example.com";
+        user.setName(name);
+        user.setEmail(email);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        Assertions.assertThat(user.getName()).isEqualTo(name);
+        Assertions.assertThat(user.getEmail()).isEqualTo(email);
+    }
+
+    @Test
+    public void remove() {
+        Long userId = 1L;
+        userService.remove(userId);
+        verify(userRepository).deleteById(userId);
+    }
+
+    @Test
+    public void testGetAll() {
+        List<User> userList = new ArrayList<>();
+        User userOne = UserTestData.getUserOne();
+        User userTwo = UserTestData.getUserTwo();
+        userList.add(userOne);
+        userList.add(userTwo);
+        when(userRepository.findAll()).thenReturn(userList);
+
+        Assertions.assertThat(userList.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void testUpdateName() {
+        User user = UserTestData.getUserOne();
+        String name = "UserNameOne";
+        user.setName(name);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        Assertions.assertThat(user.getName()).isEqualTo(name);
+    }
 
 }
