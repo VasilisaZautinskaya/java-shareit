6ecd76fdc52de79e32f5b84d771cea09d6a2967c
Василиса <vasilisaraist@yandex.ru>
2023-07-26 23:48:05 +0300

    - 80% кода покрыто;

--- src/test/java/ru/practicum/shareit/itemRequest/ItemRequestControllerTest.java
+++ src/test/java/ru/practicum/shareit/itemRequest/ItemRequestControllerTest.java
@@ -1,30 +1,14 @@
 package ru.practicum.shareit.itemRequest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.service.ItemService;
 import ru.practicum.shareit.request.controller.ItemRequestController;
-import ru.practicum.shareit.request.dto.ItemRequestDto;
-import ru.practicum.shareit.request.dto.ItemRequestResponseDto;
-import ru.practicum.shareit.request.mapper.ItemRequestMapper;
-import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.request.service.ItemRequestService;
-import ru.practicum.shareit.testData.ItemRequestTestData;
-import ru.practicum.shareit.testData.ItemTestData;
-import ru.practicum.shareit.testData.UserTestData;
 import ru.practicum.shareit.user.Service.UserService;
-import ru.practicum.shareit.user.model.User;
 
 import static org.mockito.Mockito.when;
 
